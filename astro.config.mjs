7f92a95495fccce1f1a283db1e9444ea99fc49a7
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-02-25 16:19:26 -0500

    moving image sources into src

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
+import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
-import react from '@astrojs/react'
-
-// https://astro.build/config
-import compress from 'astro-compress'
-
-// https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind(), react(), compress()],
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind(),
+        react(),
+        compress(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+    ],
 })
