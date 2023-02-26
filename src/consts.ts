e26f6168ee1a8687812685a37ed857cfad964c44
Jiahua Chen <33486899+jchen@users.noreply.github.com>
2023-02-26 11:02:00 -0500

    Make footer commit file-specific

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const GITHUB_REPO = 'jchen/site'
 export const SITE_TITLE = 'Jiahua Chen'
 export const SITE_DESCRIPTION =
     "Hey! I'm Jiahua. I love design, photography, data journalism, and occasionally a dash of formal methods."
